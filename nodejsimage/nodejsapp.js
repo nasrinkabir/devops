7a726a5323054e9d77467371a34b2fceafcf62e8
Nasrin Kabir <nasrinkabirt@gmail.com>
2020-11-10 21:21:04 +0000

    nodejs and mysql containers

---
+++ nodejsimage/nodejsapp.js
@@ -0,0 +1,47 @@
+/**
+ * @author Nasrin Kabir <nasrink@cybage.com> 
+ */
+
+const mysql = require('mysql');
+var http = require('http');
+
+// environment variables
+const PORT = process.env.PORT || 4000;
+const HOST = process.env.HOST || '0.0.0.0';
+
+
+// mysql credentials
+const connection = mysql.createConnection({
+	host: process.env.MYSQL_HOST,
+	user: process.env.MYSQL_USER, 
+	password: process.env.MYSQL_PASSWORD, 
+	database: process.env.MYSQL_DATABASE 
+});
+
+
+connection.connect(function(err) {
+      if (err) {
+        console.log("Couldn't connect to the required MySQL DB.");
+        throw err;
+      }
+    });
+
+
+// fetch Hello World string from database 
+
+var queryString="select * from helloworld;";
+var result='';
+
+connection.query(queryString, (error, rows, fields) => {
+  if (error) {
+    return console.error(error.message);
+  }
+  result = rows[0].text;
+  console.log(result);
+});
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(4000);
